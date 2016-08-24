25c77464c5744bd49ef75b6b775ad1ecff288578
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-08-24 11:51:26 +1000

    Change deck configuration

--- TestDeck.java
+++ TestDeck.java
@@ -5,11 +5,11 @@ public class TestDeck {
 
     public static void main(String[] args) {
 
-        Deck cards = new Deck();
+        Deck deck = new Deck();
 
         for(int i = 0; i < 60; ++i){
 
-            System.out.println(Deck.cards[i]);
+            System.out.println(deck.cards[i]);
         }
     }
 }
