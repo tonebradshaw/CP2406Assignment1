7b47d9b8e347c2cff9639f6fe040461eb6e2785e
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-08-24 11:52:13 +1000

    Add ArrayList tests

--- TestPlayer.java
+++ TestPlayer.java
@@ -1,17 +1,34 @@
+import java.util.ArrayList;
+
 /**
  * Created by tony on 21/08/2016.
  */
 public class TestPlayer {
 
-    static Player player;
-    static Player player1;
-
     public static void main(String[] args) {
 
-        player = new Player("Kim Pink");
-        player1 = new Player("Bob");
+        Player player = new Player("Kim Pink");
+        Player player1 = new Player("Bob");
+        ArrayList <Card> hand = player.getHand();
+        ArrayList <Card> hand1 = player1.getHand();
 
         System.out.println(player.getName());
         System.out.println(player1.getName());
+
+        Deck deck = new Deck();
+
+        for(int i = 0; i < 16; ++i){
+
+           if(i % 2 != 0) {
+               hand1.add(deck.cards[i]);
+           }else{
+               hand.add(deck.cards[i]);
+           }
+        }
+        for(int i = 0; i < 8; ++i){
+
+            System.out.print(hand.get(i).getName());
+            System.out.println("    \t" + hand1.get(i).getName());
+        }
     }
 }
